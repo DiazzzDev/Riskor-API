5ad956d4a56c2d442f041d66b71b36b8491b9627
Díaz <ed.diaz.hz@gmail.com>
2025-12-19 21:56:11 -0600

    Optimization in code v1

--- src/main/java/RiskOrganizationPTC2025/RISKOR_DevTeam/Models/DTO/DTOAccidentBodyPart.java
+++ src/main/java/RiskOrganizationPTC2025/RISKOR_DevTeam/Models/DTO/DTOAccidentBodyPart.java
@@ -1,10 +1,7 @@
 package RiskOrganizationPTC2025.RISKOR_DevTeam.Models.DTO;
 
-import RiskOrganizationPTC2025.RISKOR_DevTeam.Entities.EntityAccident;
-import RiskOrganizationPTC2025.RISKOR_DevTeam.Entities.EntityBodyPart;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
